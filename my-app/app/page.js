87c9141dd660b73e32694e17a87d79b3ee3754f5
Sravan T P <sravantp03@gmail.com>
2023-03-07 14:49:26 +0530

    added wallet connect option

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,9 +1,53 @@
+"use client";
+
 import styles from "./page.module.css";
+import CryptoDevsDAO from "../contract/cryptodevsdao.json";
+import NftMarket from "../contract/nftmarket.json";
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+import { useState, useEffect, useRef } from "react";
 
 export default function Home() {
+  const web3ModalRef = useRef();
+  const [walletConnected, setWalletConnected] = useState(false);
+
+  useEffect(() => {
+    web3ModalRef.current = new Web3Modal({
+      network: "goerli",
+      providerOptions: {},
+      disableInjectedProvider: false,
+    });
+  }, []);
+
+  const connectWallet = async () => {
+    try {
+      const instance = await web3ModalRef.current.connect();
+      const provider = new ethers.providers.Web3Provider(instance);
+      setWalletConnected(true);
+      const signer = provider.getSigner();
+      console.log(`Signer : ${await signer.getAddress()}`);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const handleConnect = async () => {
+    try {
+      await connectWallet();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <main className={styles.main}>
-      <h1>DAO</h1>
+      <button className={styles.button} onClick={handleConnect}>
+        {!walletConnected ? (
+          <span>Connect Wallet</span>
+        ) : (
+          <span>Connected</span>
+        )}
+      </button>
     </main>
   );
 }
