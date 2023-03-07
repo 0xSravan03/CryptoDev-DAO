840e40e0f6261ec8b0a287006c1096c7d03baf93
Sravan T P <sravantp03@gmail.com>
2023-03-07 15:00:05 +0530

    fix

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -2,7 +2,7 @@
 
 import styles from "./page.module.css";
 import CryptoDevsDAO from "../contract/cryptodevsdao.json";
-import NftMarket from "../contract/nftmarket.json";
+import CryptoDevsNFT from "../contract/cryptodevsnft.json";
 import { ethers } from "ethers";
 import Web3Modal from "web3modal";
 import { useState, useEffect, useRef } from "react";
@@ -23,9 +23,9 @@ export default function Home() {
     try {
       const instance = await web3ModalRef.current.connect();
       const provider = new ethers.providers.Web3Provider(instance);
-      setWalletConnected(true);
       const signer = provider.getSigner();
-      console.log(`Signer : ${await signer.getAddress()}`);
+      setWalletConnected(true);
+      return signer;
     } catch (error) {
       console.log(error.message);
     }
@@ -37,6 +37,25 @@ export default function Home() {
     } catch (error) {
       console.log(error.message);
     }
+  };
+
+  // DAO Contract Instance
+  const getDaoContractInstance = async () => {
+    const signer = await connectWallet();
+    const daoContract = new ethers.Contract(
+      CryptoDevsDAO.address,
+      CryptoDevsDAO.abi,
+      signer
+    );
+  };
+
+  const getCryptodevsNFTContractInstance = async () => {
+    const signer = await connectWallet();
+    const nftContract = new ethers.Contract(
+      CryptoDevsNFT.address,
+      CryptoDevsNFT.abi,
+      signer
+    );
   };
 
   return (
